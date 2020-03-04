a7ac19de7c5c9e25b3f9e69302d5a0c11366b700
JohannesTvS <58428953+JohannesTvS@users.noreply.github.com>
2020-03-04 09:10:52 +0100

    Add files via upload

---
+++ master.js
@@ -0,0 +1,12 @@
+var vakZijde = 40;
+
+function setup() {
+createCanvas(821,821);  // 40 breede/hooge * 20 hokjes + 21 lijnen
+  background('black');  
+}
+
+function draw() {
+  fill('green');
+  strokeline(10);
+  line(0, 0, 821, 821);
+}
