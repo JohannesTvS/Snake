f367fb4e9b01847b21d5fae9820e8c4492f58bbf
JohannesTvS <jr.terwisscha@gmail.com>
2020-03-04 08:30:04 +0000

    checkerboard niet zo goed

--- master.js
+++ master.js
@@ -1,12 +1,32 @@
 var vakZijde = 40;
+var kleur = 'green';
 
 function setup() {
-createCanvas(821,821);  // 40 breede/hooge * 20 hokjes + 21 lijnen
+createCanvas(501,501);  // 40 breede/hooge * 20 hokjes + 21 lijnen
   background('black');  
+    for (var rij = 0;rij < 500;rij += 50) {
+    for (var kolom = 0;kolom < 500;kolom += 50) {
+      fill(kleur);
+      rect(kolom,rij,50,50);
+      if (kleur == 'white') {
+        kleur = 'black';
+      }
+      else {
+        kleur = 'white';
+      }
+    }
+    if (kleur=='white') {
+      kleur='black';
+    }
+    else {
+      kleur='white';
+    }
+  }
 }
 
 function draw() {
-  fill('green');
-  strokeline(10);
-  line(0, 0, 821, 821);
+  stroke('green', 100);
+  rect(1, 1, 200, 200);
 }
+
+
