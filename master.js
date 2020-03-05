d9d740842a6c56fa0166d1913f503b0740f6141c
JohannesTvS <jr.terwisscha@gmail.com>
2020-03-05 21:11:27 +0000

    hij beweegt en inputs worden geregistreerd

--- master.js
+++ master.js
@@ -1,36 +1,69 @@
 var vakZijde = 40;
 var aantalHokjes = 20;
-var spelVlakBreedte = aantalHokjes*vakZijde;
+var spelVlakBreedte = aantalHokjes * vakZijde;
 var beginLengte = 3;
+var richting = "links";
 
 function setup() {
-createCanvas(spelVlakBreedte+1,spelVlakBreedte+1);
-  background('black');  
-      createCheckers(spelVlakBreedte, vakZijde);
+  createCanvas(spelVlakBreedte + 1, spelVlakBreedte + 1);
+  frameRate(1);
+  background('black');
   slang = new Snake();
+
+}
+
+
+function draw(){
+    createCheckers(spelVlakBreedte, vakZijde);
+    slang.beweeg(richting);
     slang.teken();
-}
-
-
-function draw() {
+console.log(richting);
 
 }
 
 
-function createCheckers (spelVlakBreedte, vakZijde) {
-    for (var rij = 0;rij < spelVlakBreedte;rij += vakZijde) {
-        for (var kolom = 0;kolom < spelVlakBreedte;kolom += vakZijde) {
-            if ((rij % (vakZijde*2) == 0 && kolom % (vakZijde*2) != 0) || (kolom % (vakZijde*2) == 0 && rij % (vakZijde*2) != 0)) {
-                fill('black');
-                rect(kolom,rij,vakZijde,vakZijde);
-            }
-            else {
-            fill('white');
-            rect(kolom,rij,vakZijde,vakZijde);
-            }
-        }
+function createCheckers(spelVlakBreedte, vakZijde) {
+  for (var rij = 0; rij < spelVlakBreedte; rij += vakZijde) {
+    for (var kolom = 0; kolom < spelVlakBreedte; kolom += vakZijde) {
+      if ((rij % (vakZijde * 2) == 0 && kolom % (vakZijde * 2) != 0) || (kolom % (vakZijde * 2) == 0 && rij % (vakZijde * 2) != 0)) {
+        fill('black');
+        rect(kolom, rij, vakZijde, vakZijde);
+      } else {
+        fill('white');
+        rect(kolom, rij, vakZijde, vakZijde);
+      }
     }
+  }
 }
 
+function keyPressed() {
+  switch (keyCode) {
+    case 38:
+      richting = "omhoog";
+      break;
+    case 87:
+      richting = "omhoog";
+      break;
+    case 37:
+      richting = "links";
+      break;
+    case 65:
+      richting = "links";
+      break;
+    case 39:
+      richting = "rechts";
+      break;
+    case 68:
+      richting = "rechts";
+      break;
+    case 40:
+      richting = "omlaag";
+      break;
+    case 83:
+      richting = "omlaag";
+      break;
+    default:
+      break;
+  }
 
-
+}
--- snake.js
+++ snake.js
@@ -15,4 +15,26 @@ class Snake {
       rect(this.lichaam[i], this.lichaam[i + 1], vakZijde, vakZijde);
     }
   }
+
+
+  beweeg(richting) {
+
+
+    switch (richting) {
+      case "omhoog":
+        break;
+      case "links":
+        this.lichaam[0] = this.lichaam[0] - vakZijde;
+        
+        this.lengte = this.lichaam.length;  //deze drie moeten in een functie ofzo
+        for (let i = 2; i < this.lengte; i++) {    //maar snap niet hoe dat in een class 
+          this.lichaam[i] = this.lichaam[i - 2]    //zou moeten werken
+        }
+        break;
+      case "omhoog":
+        break;
+      case "omhoog":
+        break;
+    }
+  }
 }
